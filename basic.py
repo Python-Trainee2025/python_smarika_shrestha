c5efd847ba0d93795a1207eaaced422cfe12b439
Smarika Shrestha <smshrestha@infics.com>
2025-10-26 13:39:22 +0545

    Initial COmmit

---
+++ basic.py
@@ -0,0 +1,14 @@
+print('hello world1')
+print('hello world2')
+
+'''
+help('keywords') helps you see keywords
+semicolon
+identation
+case sensitive
+comment # or triple colon
+naming convention
+variables lower case
+class upper case
+'''
+
